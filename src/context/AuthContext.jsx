d5affca1435657476cbf4c4ca2cb34eec944f973
Damian Valencia <damival.32@gmail.com>
2025-09-18 22:13:36 -0600

    Update AuthContext.jsx
    Co-Authored-By: Geraldine Justo  <185227639+pjusto930@users.noreply.github.com>

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,6 +3,8 @@ import authService from '../services/authService';
 
 const AuthContext = createContext();
 
+export { AuthContext };
+
 export const useAuth = () => {
     const context = useContext(AuthContext);
     if (!context) {
